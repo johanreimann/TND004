17438b21b7bd325b41532289ed1a7379513c8af6
CarolineGard <caroline.gard@hotmail.com>
2015-05-08 14:26:31 +0200

    Update hashTable.cpp

--- Lab2/hashTable.cpp
+++ Lab2/hashTable.cpp
@@ -27,7 +27,6 @@ bool isPrime( int n )
     return true;
 }
 
-
 //Return a prime number at least as large as n
 int nextPrime( int n )
 {
@@ -39,14 +38,15 @@ int nextPrime( int n )
     return n;
 }
 
-
 // Constructor
 // IMPLEMENT
 HashTable::HashTable(int table_size, HASH f)
     : size(nextPrime(table_size)), h(f), nItems(0)
 {
     hTable = new Item*[size];
-    hTable[0] = nullptr;
+
+    for(int i = 0; i < size; i++)
+        hTable[i] = nullptr;
 }
 
 
@@ -54,14 +54,14 @@ HashTable::HashTable(int table_size, HASH f)
 // IMPLEMENT
 HashTable::~HashTable()
 {
-    Item* temp;
-
-    for(int i = 0; i < size; i++)
-    {
-        temp = hTable[i];
-        delete temp;
-    }
-    delete[] hTable;
+//    Item* temp;
+//
+//    for(int i = 0; i < size; i++)
+//    {
+//        temp = hTable[i];
+//        delete temp;
+//    }
+    delete hTable;
 }
 
 
@@ -81,13 +81,22 @@ int HashTable::find(string key) const
     int counter = 0;
 
     do {
-        if(hTable[slot]->key == key)
+        if(hTable[slot] == nullptr || hTable[slot] == Deleted_Item::get_Item())
+        {
+            counter++;
+            slot++;
+        }
+
+        else if(hTable[slot]->key == key)
             return hTable[slot]->value;
 
-        counter++;
-        slot++;
+        else
+        {
+            counter++;
+            slot++;
+        }
 
-        if(slot == (size-1))
+        if(slot == (size))
             slot = 0;
 
     } while(counter < size);
@@ -105,24 +114,32 @@ void HashTable::insert(string key, int v)
     bool test = false;
     int slot = h(key, size);
     int counter = 0;
-
     //Rehash
     if((loadFactor()) >= MAX_LOAD_FACTOR)
         reHash();
 
     //testa om key redan finns
     do {
-        if(hTable[slot]->key == key)
+        if(hTable[slot] == nullptr || hTable[slot] == Deleted_Item::get_Item())
+        {
+            slot++;
+            counter++;
+        }
+
+        else if(hTable[slot]->key == key)
         {
             hTable[slot]->value = v;
             test = true;
         }
-        slot++;
-        counter++;
-
+        else
+        {
+            slot++;
+            counter++;
+        }
         //börja om från början i arrayen
         if(slot == (size))
             slot = 0;
+
 
     } while (!test && ( counter < size ));
 
@@ -133,40 +150,19 @@ void HashTable::insert(string key, int v)
     while (!test)
     {
         //om key == null
-        if (!hTable[slot])
+        if (hTable[slot] == nullptr || hTable[slot] == Deleted_Item::get_Item())
         {
             hTable[slot] = new Item(key, v);
             nItems++;
             break;
         }
-        //om deleted key = ""
-        else if(hTable[slot]->key == "" && hTable[slot]->value == -1 )
-        {
-            hTable[slot] = new Item(key, v);
-            nItems++;
-            break;
-            //måste man ta bort Item("", -1) ?
-        }
+
         else
+        {
             slot++;
+        }
     }
 }
-/*
-void HashTable::fileInsert(string filename)
-{
-    string word;
-    ifstream infile;
-	infile.open("test files/" + filename);
-
-	while(infile >> word) // To get you all the lines.
-    {
-    	cleanUp(word);
-        hTable[word];
-    }
-	infile.close();
-
-    display(cout);
-}*/
 
 //Remove Item with key
 //If an Item with key belongs to the table then return true,
@@ -178,14 +174,22 @@ bool HashTable::remove(string key)
     int counter = 0;
 
     do {
-        if (hTable[slot]->key == key) {
+        if(hTable[slot] == nullptr || hTable[slot] == Deleted_Item::get_Item())
+        {
+            counter++;
+            slot++;
+        }
+        else if(hTable[slot]->key == key)
+        {
             hTable[slot] =  Deleted_Item::get_Item();
             nItems--;
             return true;
         }
-        counter++;
-        slot++;
-
+        else
+        {
+            counter++;
+            slot++;
+        }
         //börja om från början i arrayen
         if(slot == (size-1))
             slot = 0;
@@ -223,25 +227,45 @@ void HashTable::display(ostream& os)
 }
 
 
+int HashTable::get_number_total_words() const
+{
+    int words = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(hTable[i] == nullptr || hTable[i] == Deleted_Item::get_Item())
+            continue;
+
+        else
+            words += hTable[i]->value;
+    }
+     return words;
+}
+
 //Display the table to stream os
 // IMPLEMENT
 ostream& operator<<(ostream& os, const HashTable& T)
 {
+    cout << endl << "Number of words in the file = " << T.get_number_total_words() << endl;
+    cout << "Number unique  words in the file = " << T.get_number_OF_items() << endl << endl;;
+    cout << "Frequency table ..." << endl << endl;
     for(int i = 0; i < T.size; i++)
     {
-        if(!T.hTable[i])
-            continue;
-
-        else if(T.hTable[i]->key == "" && T.hTable[i]->value == -1)
+        if(T.hTable[i] == nullptr || T.hTable[i] == Deleted_Item::get_Item())
             continue;
 
         else
-            os << "Key: " << T.hTable[i]->key << " Value: " << T.hTable[i]->value << endl;
+            os << "Key: " << setw(13) << left << T.hTable[i]->key << " Value: " << T.hTable[i]->value << endl;
     }
+/*
+    cout << endl << "Number of words in the file = " << T.get_number_total_words() << endl;
+    cout << "Number unique  words in the file = " << T.get_number_OF_items() << endl << endl;
+    cout << "Frequency table ..." << endl << endl; */
+
     return os;
 }
 
-int &HashTable::operator[](const string w)
+
+void HashTable::operator[](const string w)
 {
     string word = w;
     cleanUp(word);
@@ -256,57 +280,46 @@ int &HashTable::operator[](const string w)
 
     //testa om key redan finns
     do {
-        cout << "4" << endl;
-        if (!hTable[slot])
+        if (hTable[slot] == nullptr || hTable[slot] == Deleted_Item::get_Item())
         {
             slot++;
             counter++;
-            continue;
         }
         else if(hTable[slot]->key == word)
         {
-            cout << "6" << endl;
             hTable[slot]->value++;
             test = true;
         }
-        slot++;
-        counter++;
-
-        cout << "8" << endl;
+        else
+        {
+            slot++;
+            counter++;
+        }
         //börja om från början i arrayen
+
         if(slot == (size))
             slot = 0;
 
     } while (!test && ( counter < size ));
 
-    cout << "jeeeeeh" << endl;
     //Fall 1: om key redan finns
     //Fall 2: Om slot för key = null
     slot = h(word, size);
-    cout << "7" << endl;
     while (!test)
     {
-        cout << "5" << endl;
         //om key == null
-        if (!hTable[slot])
+        if (!hTable[slot] || hTable[slot] == Deleted_Item::get_Item())
         {
             hTable[slot] = new Item(word, 1);
             nItems++;
             break;
-        }
-        //om deleted key = ""
-        else if(hTable[slot]->key == "" && hTable[slot]->value == -1 )
-        {
-            hTable[slot] = new Item(word, 1);
-            nItems++;
-            break;
-            //måste man ta bort Item("", -1) ?
         }
         else
             slot++;
     }
-    return hTable[slot]->value;
+//    return hTable[slot]->value;
 }
+
 
 void HashTable::cleanUp(string& w)
 {
@@ -330,11 +343,17 @@ void HashTable::reHash()
     int newCap = nextPrime(size*2);
     Item** newHashTable = new Item*[newCap];
 
+    for(int i = 0; i < newCap; i++)
+        newHashTable[i] = nullptr;
+
     for(int i = 0; i < size; ++i)
-    {
         newHashTable[i] = hTable[i];
-    }
-    delete[] hTable;
+
+    delete hTable;
     size = newCap;
     hTable = newHashTable;
+
+/*
+    cout << "Rehashing... " << endl;
+    cout << "New Size: " << newCap << endl << endl;*/
 }
