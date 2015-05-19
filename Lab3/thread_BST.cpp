d01381c9244f96a7cf10ab41af74986ecc0d6333
johanreimann <reimann.johan@gmail.com>
2015-05-19 18:40:18 +0200

    Updated thread_BST

--- Lab3/thread_BST.cpp
+++ Lab3/thread_BST.cpp
@@ -20,14 +20,13 @@ using namespace std;
 BST_threaded::BST_threaded()
  : counter(0)
 {
-    root = new Node(make_pair("", 0), root, root);
+    root = new Node(ELEMENT(), root, root);
 }
 
 
 //destructor
 BST_threaded::~BST_threaded()
 {
-    //ADD CODE
     delete root;
 }
 
@@ -36,10 +35,8 @@ BST_threaded::~BST_threaded()
 bool BST_threaded::empty() const
 {
     if(root->l_thread)
-    {
-        cout << "empty" << endl;
         return true;
-    }
+
     return false;
 }
 
@@ -82,28 +79,101 @@ void BST_threaded::remove(string key)
 //then an ELEMENT (key,0) is inserted and a reference to it is returned
 ELEMENT& BST_threaded::operator[](string key)
 {
-    //ADD CODE
-    static ELEMENT e("", 0); //MUST remove this code
+    BiIterator it = find(key);
 
-    return e; //MUST remove this code
+    if(it == end())
+    {
+        insert(ELEMENT(key, 0));
+        it = find(key);
+    }
+
+    //VARFÖR FUNKAR DET INTE UTAN ATT DEKLARERA DESSA?CP
+//    string k = it->first;
+//    int t = it->second;
+
+//    ELEMENT f(it->first, it->second);
+
+//    ELEMENT f(k, t);
+
+    return *it; //MUST remove this code
 }
 
 
 //Find the node in the BST storing key
 //Return a BiIterator referring to the node storing the key, if the key is found.
 //Otherwise, return this->end().
+
+//Stega ökande: lägsta till största
 BiIterator BST_threaded::find(string key) const
 {
-    //ADD CODE
-    return end();
+    Node *tmp = root->left->find(key);
+
+    if(tmp != nullptr)
+        return BiIterator(tmp);
+
+    else
+        return end();
+
+   /* Node *tmp = root->left;
+    BiIterator it = begin(); //ELEMENTET
+    bool ins = false;
+
+    for (it; it != end(); it++)
+    {
+        if(it->first == key)
+            return it;
+    }*/
+
+//    cout << "KEY " << key << endl;
+/*
+    while(!ins)
+    {
+
+//        cout << " IT: " << it->value.first << endl;
+//        cout << "KEY:  " << key << endl;
+
+        if(key == it->first)
+        {
+            cout << "2" << endl;
+            ins = true;
+            cout << "HÄR ÄR IT: " << it->first << endl;
+            return it;
+        }
+
+
+        else if(key < it->first && !it->)
+        {
+//            cout << "3" << endl;
+            it = it->left;
+//             cout << "efter 3: " << it->value.first << endl;
+        }
+
+
+        else if(key > it->value.first && !it->r_thread)
+        {
+//            cout << "4" << endl;
+            it = it->right;
+        }
+
+
+        else if((key < it->value.first && it->l_thread) || (key > it->value.first && it->r_thread))
+        {
+            ins = true;
+            return end();
+        }
+
+    }*/
+
+
 }
 
 
-//Return an iterator referring to the first node in the inorder traversal of the BST
+//Return an iterator referring to the first node in the inorder traversal(genomkorsande) of the BST
 BiIterator BST_threaded::begin() const
 {
-    //ADD CODE
-    return end();
+    Node *node = root->findMin();
+    cout << "MIN: " << node->value.first << endl;
+    return BiIterator(node);
 }
 
 
