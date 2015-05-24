f860be0417320ec365255123472100bacd9b7fed
johanreimann <reimann.johan@gmail.com>
2015-05-24 19:07:01 +0200

    Update node.cpp

--- Lab3/node.cpp
+++ Lab3/node.cpp
@@ -144,11 +144,10 @@ bool Node::tree_insert(Node* n, Node* tmp, ELEMENT v)
 //isRight==true: this node is right child of parent
 bool Node::remove(string key, Node* parent, bool isRight)
 {
-    //fall 1: vänster nod med både vänster och höger child
-    if (!isRight && !l_thread && !r_thread)
+    if (!l_thread && !r_thread)
     {
         Node* tmp = this;
-        tmp->value = tmp->right->findMin()->value;
+        value = tmp->right->findMin()->value;
         if(!tmp->right->l_thread)
         {
             tmp = tmp->right;
@@ -160,26 +159,6 @@ bool Node::remove(string key, Node* parent, bool isRight)
         else
             tmp->right->removeMe(tmp, true);
     }
-
-    //fall 2: höger nod med både vänter och höger child
-    else if (isRight && !l_thread && !r_thread)
-    {
-        Node* tmp = this;
-        tmp->value = tmp->left->findMin()->value;
-        if(!tmp->left->l_thread)
-        {
-            tmp = tmp->left;
-            while(!tmp->left->l_thread)
-                tmp = tmp->left;
-
-            tmp->left->removeMe(tmp, false);
-        }
-        else
-        {
-            vad ska vi göra här?
-        }
-    }
-
     else
         removeMe(parent, isRight);
 
@@ -204,20 +183,20 @@ void Node::removeMe(Node* parent, bool isRight)
     if (!isRight && l_thread && !r_thread)
     {
         parent->left = right;
-        right->left = left;
+        right->findMin()->left = left;
     }
 
     //1b: a left child with only a left child
     else if (!isRight && !l_thread && r_thread)
     {
         parent->left = left;
-        left->right = right;
+        left->findMax()->right = right;
     }
 
     //1c: a left child with no children
     else if (!isRight && l_thread && r_thread)
     {
-        parent->left = parent->left->left;
+        parent->left = left; //parent->left->left;
         parent->l_thread = true;
     }
 
@@ -225,14 +204,14 @@ void Node::removeMe(Node* parent, bool isRight)
     else if (isRight && l_thread && !r_thread)
     {
         parent->right = right;
-        right->left = left;
+        right->findMin()->left = left;
     }
 
     //2b: a right child with only a left child
     else if (isRight && !l_thread && r_thread)
     {
         parent->right = left;
-        left->right = right;
+        left->findMax()->right = right;
     }
 
     //2c: a right child with no children
@@ -241,6 +220,8 @@ void Node::removeMe(Node* parent, bool isRight)
         parent->right = right;
         parent->r_thread = true;
     }
+    this->l_thread = this->r_thread = true;
+    delete this;
 }
 
 
@@ -265,13 +246,10 @@ Node* Node::find(string key)
 //return parent(node) of found key
 Node* Node::find_parent(string key, bool &is_right)
 {
-    cout << "p" << endl;
     if (key < value.first && !l_thread)
     {
-        cout << "k" << endl;
         if (left->value.first == key)
         {
-            cout << "m" << endl;
             is_right = false;
             return this; //return parent
         }
@@ -281,17 +259,14 @@ Node* Node::find_parent(string key, bool &is_right)
 
     else if (key > value.first && !r_thread)
     {
-        cout << "n" << endl;
         if (right->value.first == key)
         {
-            cout << "ö" << endl;
             is_right = true;
             return this;
         }
         else
             return right->find_parent(key, is_right);
     }
-
     return nullptr; //hittar ej
 }
 
